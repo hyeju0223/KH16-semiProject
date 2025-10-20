6dad920358c028e50cdeb1b16315886696d3f49f
hyeju0223 <juya_0223@naver.com>
2025-10-20 10:22:35 +0900

    프로젝트 생성(강사)

---
+++ muzic/src/test/java/com/muzic/MuzicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
